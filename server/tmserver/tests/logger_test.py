08cce78bde2af451b94bd17dd53e63452090182d
vilmibm <nks@neongrid.space>
2018-04-10 22:28:56 -0700

    add logger tests

--- server/tmserver/tests/logger_test.py
+++ server/tmserver/tests/logger_test.py
@@ -1,28 +1,49 @@
+import logging
 import unittest.mock as mock
 import unittest
 
+from ..logs import PGHandler, get_logger
 from ..migrations import reset_db
 from ..models import Log
 
-# TODO these tests should test the postgresql logging handler, not that we log where we expect to log.
 
 class TestPGHandler(unittest.TestCase):
     def setUp(self):
+        self.pgh = PGHandler()
+        self.mock_log_record = mock.Mock()
+        self.mock_log_record.getMessage.return_value = 'sweet'
+        self.mock_log_record.levelname = 'INFO'
         reset_db()
 
     def test_respects_env(self):
-        pass
+        self.assertEqual('test', self.pgh.env)
 
     def test_sets_created_at(self):
-        pass
+        self.pgh.emit(self.mock_log_record)
+        logs = Log.select()
+        self.assertIsNotNone(logs[0].created_at)
 
     def test_levels(self):
-        pass
+        self.pgh.emit(self.mock_log_record)
+        logs = Log.select()
+        self.assertEqual(logs[0].level, 'INFO')
 
+    def test_msg(self):
+        self.pgh.emit(self.mock_log_record)
+        logs = Log.select()
+        self.assertEqual(logs[0].raw, 'sweet')
 
-# TODO this testcase ensures that get_logger works as expected
 
 class TestLogging(unittest.TestCase):
     def test_debug_ignores_pg(self):
-        pass
+        with mock.patch('logging.getLogger') as m:
+            logger = get_logger(debug=True)
+
+        self.assertFalse(m.return_value.addHandler.called)
+
+    def test_defaults_to_pg(self):
+        with mock.patch('logging.getLogger') as m:
+            logger = get_logger()
+        self.assertTrue(m.return_value.addHandler.called)
+
 
