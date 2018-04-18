a46d2d73148472d30b0d3f2a55ef51e081ffad11
vilmibm <nks@neongrid.space>
2018-04-17 21:03:23 -0700

    dirty WIP: starting on object ownership

---
+++ server/tmserver/tests/room_test.py
@@ -0,0 +1,14 @@
+from ..models import User, Room
+
+from .tm_test_case import TildemushTestCase
+
+class RoomTest(TildemushTestCase):
+    # as a player i want to create a room
+    # as a player i want to see what rooms i have ownership over
+    pass
+
+class LocationTest(TildemushTestCase):
+    # as a player i want to know what room i'm currently in
+    # as a player i want to know what rooms i can go to and how to get to them
+    # as a player i want to know how to get back to a room i was just in
+    pass
