96ff2c6f05afdb8fcb7223ab08865f27fb2de0ae
vilmibm <nks@neongrid.space>
2018-05-15 23:59:51 -0700

    finish async server tests

--- server/tmserver/tests/async_test.py
+++ server/tmserver/tests/async_test.py
@@ -25,7 +25,10 @@ def mock_logger():
 @pytest.fixture(autouse=True)
 def start_server(event_loop, mock_logger):
     gs = GameServer(GameWorld, loop=event_loop, logger=mock_logger, port=5555)
-    asyncio.ensure_future(gs._get_ws_server(), loop=event_loop)
+    server_future = gs._get_ws_server()
+    asyncio.ensure_future(server_future, loop=event_loop)
+    yield
+    server_future.ws_server.server.close()
 
 @pytest.fixture
 async def client(event_loop):
@@ -34,12 +37,54 @@ async def client(event_loop):
     await client.close()
 
 @pytest.mark.asyncio
+async def test_garbage(event_loop, mock_logger, client):
+    await client.send('GARBAGE')
+    msg = await client.recv()
+    assert msg == 'ERROR: message not understood'
+
+@pytest.mark.asyncio
 async def test_ping(event_loop, mock_logger, client):
     await client.send('PING')
     msg = await client.recv()
     assert msg == 'PONG'
 
+@pytest.mark.asyncio
+async def test_registration_success(event_loop, mock_logger, client):
+    await client.send('REGISTER vilmibm:foobarbazquux')
+    msg = await client.recv()
+    assert msg == 'REGISTER OK'
 
-# TODO test registration
-# TODO test login
-# TODO test game commands
+@pytest.mark.asyncio
+async def test_registration_error(event_loop, mock_logger, client):
+    await client.send('REGISTER vilmibm:foo')
+    msg = await client.recv()
+    assert msg == 'ERROR: password too short'
+
+@pytest.mark.asyncio
+async def test_login_success(event_loop, mock_logger, client):
+    await client.send('REGISTER vilmibm:foobarbazquux')
+    await client.recv()
+    await client.send('LOGIN vilmibm:foobarbazquux')
+    msg = await client.recv()
+    assert msg == 'LOGIN OK'
+
+@pytest.mark.asyncio
+async def test_login_error(event_loop, mock_logger, client):
+    await client.send('REGISTER vilmibm:foobarbazquux')
+    await client.recv()
+    await client.send('LOGIN evilmibm:foobarbazquux')
+    msg = await client.recv()
+    assert msg == 'ERROR: no such user'
+
+
+@pytest.mark.asyncio
+async def test_game_command(event_loop, mock_logger, client):
+    await client.send('REGISTER vilmibm:foobarbazquux')
+    await client.recv()
+    await client.send('LOGIN vilmibm:foobarbazquux')
+    await client.recv()
+    await client.send('COMMAND say hello')
+    msg = await client.recv()
+    assert msg == 'COMMAND OK'
+    msg = await client.recv()
+    assert msg == 'a gaseous cloud says hello'
