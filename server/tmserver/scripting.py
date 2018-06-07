b05550e87456ac1e94cab200fb7e1b512dc9886a
vilmibm <nks@neongrid.space>
2018-06-06 22:03:11 -0700

    move stuff around

--- server/tmserver/scripting.py
+++ server/tmserver/scripting.py
@@ -72,6 +72,45 @@ class ScriptedObjectMixin:
     handling, you aren't interested in a GameObject's persisted data and
     vice-versa.
     """
+
+    @property
+    def engine(self):
+        if not hasattr(self, '_engine'):
+            if self.script_revision is None:
+                self._engine = ScriptEngine()
+            else:
+                try:
+                    self._engine = self._execute_script(self.script_revision.code)
+                except Exception as e:
+                    raise WitchException(
+                        ';_; There is a problem with your witch script: {}'.format(e))
+
+        return self._engine
+
+    def handle_action(self, game_world, sender_obj, action, action_args):
+        self._ensure_world(game_world)
+        # TODO there are *horrifying* race conditions going on here if set_data
+        # and get_data are used in separate transactions. Call handler inside
+        # of a transaction:
+        return self.engine.handler(game_world, action)(self, sender_obj, action_args)
+
+    # say, set_data, and get_data are part of the WITCH scripting API. that
+    # should probably be explicit somehow?
+
+    def say(self, message):
+        self.game_world.dispatch_action(self, 'say', message)
+
+    # TODO I may want to forbid getting/setting things not originally declared
+    # via ensure_data. This might help newer programmers catch typos in WITCH
+    # scripts. For now, eh.
+    # lol this would have saved me some debugging earlier when i mixed up - and _
+    def set_data(self, key, value):
+        self.data[key] = value
+        self.save()
+
+    def get_data(self, key):
+        return self.get_by_id(self.id).data.get(key)
+
     def _execute_script(self, witch_code):
         """Given a pile of script revision code, this function prepends the
         (witch) macro definition and then reads and evals the combined code."""
@@ -100,42 +139,3 @@ class ScriptedObjectMixin:
     def _ensure_world(self, game_world):
         if not hasattr(self, 'game_world'):
             self.game_world = game_world
-
-    # say, set_data, and get_data are part of the WITCH scripting API. that
-    # should probably be explicit somehow?
-
-    def say(self, message):
-        self.game_world.dispatch_action(self, 'say', message)
-
-    # TODO I may want to forbid getting/setting things not originally declared
-    # via ensure_data. This might help newer programmers catch typos in WITCH
-    # scripts. For now, eh.
-    # lol this would have saved me some debugging earlier when i mixed up - and _
-    def set_data(self, key, value):
-        self.data[key] = value
-        self.save()
-
-    def get_data(self, key):
-        return self.get_by_id(self.id).data.get(key)
-
-    def handle_action(self, game_world, sender_obj, action, action_args):
-        self._ensure_world(game_world)
-        # TODO there are *horrifying* race conditions going on here if set_data
-        # and get_data are used in separate transactions. Call handler inside
-        # of a transaction:
-        return self.engine.handler(game_world, action)(self, sender_obj, action_args)
-
-
-    @property
-    def engine(self):
-        if not hasattr(self, '_engine'):
-            if self.script_revision is None:
-                self._engine = ScriptEngine()
-            else:
-                try:
-                    self._engine = self._execute_script(self.script_revision.code)
-                except Exception as e:
-                    raise WitchException(
-                        ';_; There is a problem with your witch script: {}'.format(e))
-
-        return self._engine
