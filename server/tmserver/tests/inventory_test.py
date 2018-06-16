182fcc474036e1476b4af431588503d77fc2ca87
vilmibm <nks@neongrid.space>
2018-06-15 22:16:09 -0700

    get unit tests

--- server/tmserver/tests/inventory_test.py
+++ server/tmserver/tests/inventory_test.py
@@ -2,40 +2,42 @@ from unittest import mock
 
 from .tm_test_case import TildemushTestCase
 from ..errors import ClientException
-from ..models import UserAccount, GameObject
+from ..models import UserAccount, GameObject, Permission
 from ..world import GameWorld
 
-def setup_scene():
-    foyer = GameObject.get(GameObject.shortname=='foyer')
-    god = UserAccount.get(UserAccount.username=='god')
-
-    vil_ua = UserAccount.create(
-        username='vilmibm',
-        password='foobarbazquux')
-
-    can = GameObject.create_scripted_object(
-        'item', god, 'can-god', dict(
-            name='A Rusted Tin Can',
-            description='The label has been torn off. A patch of adhesive is still a little sticky, though the metal is all rusted over. Something about it is unsettling.'))
-    bag = GameObject.create_scripted_object(
-        'item', god, 'bag-god', dict(
-            name='A Garbage Bag',
-            description="It's just a garbage bag. The black, glossy kind. You wonder how much stuff you could fit in there."))
-
-    # TODO initial perms? by default, things are pretty open. i'll restrict as
-    # needed unless that sucks...
-    GameWorld.put_into(foyer, can)
-    GameWorld.put_into(foyer, bag)
-    GameWorld.put_into(foyer, vil_ua.player_obj)
-
-    return vil_ua.player_obj
-
-@mock.patch('tmserver.world.GameWorld.user_hears')
-class GetTest(TildemushTestCase):
+class InventoryTestCase(TildemushTestCase):
     def setUp(self):
         super().setUp()
-        self.vil = setup_scene()
+        foyer = GameObject.get(GameObject.shortname=='foyer')
+        god = UserAccount.get(UserAccount.username=='god')
 
+        vil_ua = UserAccount.create(
+            username='vilmibm',
+            password='foobarbazquux')
+
+        can = GameObject.create_scripted_object(
+            'item', god, 'can-god', dict(
+                name='A Rusted Tin Can',
+                description='The label has been torn off. A patch of adhesive is still a little sticky, though the metal is all rusted over. Something about it is unsettling.'))
+        bag = GameObject.create_scripted_object(
+            'item', god, 'bag-god', dict(
+                name='A Garbage Bag',
+                description="It's just a garbage bag. The black, glossy kind. You wonder how much stuff you could fit in there."))
+
+        # TODO initial perms? by default, things are pretty open. i'll restrict as
+        # needed unless that sucks...
+        GameWorld.put_into(foyer, can)
+        GameWorld.put_into(foyer, bag)
+        GameWorld.put_into(foyer, vil_ua.player_obj)
+
+        self.foyer = foyer
+        self.god = god
+        self.vil = vil_ua.player_obj
+        self.bag = bag
+        self.can = can
+
+@mock.patch('tmserver.world.GameWorld.user_hears')
+class GetTest(InventoryTestCase):
     def test_obj_not_found(self, _):
         with self.assertRaisesRegex(
                 ClientException,
@@ -43,19 +45,36 @@ class GetTest(TildemushTestCase):
             GameWorld.handle_get(self.vil, 'shoe')
 
     def test_obj_denied(self, _):
-        pass
+        self.can.perms.carry = Permission.OWNER
+        self.can.perms.save()
 
-    def test_success(self, _):
-        pass
+        with self.assertRaisesRegex(
+                ClientException,
+                'You grab a hold of A Rusted Tin Can but'):
+            GameWorld.handle_get(self.vil, 'can')
 
-    def test_ambiguity(self, _):
-        pass
+    def test_success(self, mock_hears):
+        GameWorld.handle_get(self.vil, 'can')
+        assert self.can in self.vil.contains
+        assert mock_hears.called
+
+    def test_ambiguity(self, mock_hears):
+        shiny_can = GameObject.create_scripted_object(
+            'item', self.god, 'can-shiny-god', dict(
+                name='A New, Shiny Tin Can',
+                description="A new can. Not even adhesive on this. You can see yourself in its reflection but you're all ripply."))
+        GameWorld.put_into(self.foyer, shiny_can)
+        assert shiny_can in self.foyer.contains
+        assert self.can in self.foyer.contains
+        assert shiny_can.fuzzy_match('can')
+        assert self.can.fuzzy_match('can')
+        GameWorld.handle_get(self.vil, 'can')
+        assert self.can in self.vil.contains
+        assert shiny_can not in self.vil.contains
+        assert mock_hears.called
 
 
-class DropTest(TildemushTestCase):
-    def test_obj_not_found(self):
-        pass
-
+class DropTest(InventoryTestCase):
     def test_success(self):
         pass
 
